d7813aaf09ced831a2cf204f9bccaa13c4232a3c
Oscar <oscargm28@hotmail.com>
2022-01-20 15:01:30 +0100

    data types and more

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,6 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
-	fmt.Println("Get your tickets here to attend")
+	/* Syntactic Sugar <varName> := value */
+	conferenceName := "Go Conference";
+	const conferenceTickets uint8 = 50;
+	var remainingTickets uint8 = conferenceTickets;
+
+	/* %T me dirá el data type de una variable/constante */
+	fmt.Printf("conferenceTickets type is %T while conferenceName's data type is %T\n", conferenceTickets, conferenceName);
+
+	/* %v formatea por defecto */
+	fmt.Printf("Welcome to our %v booking application\n",conferenceName);
+	fmt.Printf("We have total of %v tickets available and %v are still available.\n",conferenceTickets,remainingTickets);
+
+	fmt.Println("Get your tickets here to attend");
+
+	var userName string;
+	var userTickets int;
+	/* ask user for their name */
+	userName = "Tom"
+	userTickets =2;
+	fmt.Printf("User %v has booked %v tickets\n",userName,userTickets);
+
 }
